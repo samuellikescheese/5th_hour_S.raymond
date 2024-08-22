304bbb173137b41b7265b065db05e2796cfb2b3b
samuel_raymond <sr128226@student.wpsok.org>
2024-08-22 13:00:06 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,15 @@
+#samuel raymond
+#5th hour
+#HW2
+
+print("hello world")
+
+print("what is your name")
+name = input()
+
+print("give me a number")
+x = input()
+print("give me a other number")
+y = input()
+sum= int(x) + int(y)
+print(sum)
