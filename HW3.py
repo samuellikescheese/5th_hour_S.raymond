4e5551d4a6458d728a684a60df77aef11363262a
samuel_raymond <sr128226@student.wpsok.org>
2024-08-29 13:02:05 -0500

    added HW3

---
+++ HW3.py
@@ -0,0 +1,15 @@
+#samuel raymond
+#6th hour
+#HW3
+
+print("HELLO WORLDd")
+
+namelist = ["sam", "gage", "payton", "timmy", "dave"]
+namelist.append("lake")
+print (namelist[3])
+
+num= [1, 7, 4, 3]
+num.insert(1, 2)
+print (num)
+num.sort()
+print(num[0]+num[1]+num[2])
