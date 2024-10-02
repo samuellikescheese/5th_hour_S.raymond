8ca2155e059b7753e16c7b80aa3dce2df1ff938a
samuel_raymond <sr128226@student.wpsok.org>
2024-10-02 13:14:28 -0500

    added HW6.py

--- HW6.py
+++ HW6.py
@@ -7,25 +7,63 @@ num = int(input())
 
 if num % 2 == 0:
     if num % 3 == 0:
-        print("your number is divesible by 2")
-        x = num / 2
-        print (f"{num} / 2 = {x}")
+        if num % 5 == 0:
+            print("your number is divesible by 2")
+            x = num / 2
+            print(f"{num} / 2 = {x}")
+            print("your number is divesible by 3")
+            y = num / 3
+            print(f"{num} / 3 = {y}")
+            print("your number is divesible by 5")
+            z = num / 5
+            print(f"{num} / 3 = {z}")
+        else:
+            print("it is not divesible by 5")
+            print("your number is divesible by 2")
+            x = num / 2
+            print (f"{num} / 2 = {x}")
+            print("your number is divesible by 3")
+            y = num / 3
+            print (f"{num} / 3 = {y}")
+    else:
+        if num % 5 == 0 :
+            print("it is not divesible by 3")
+            print("your number is divesible by 2")
+            x = num / 2
+            print(f"{num} / 2 = {x}")
+            print("your number is divesible by 5")
+            z = num / 5
+            print(f"{num} / 5 = {z}")
+        else :
+            print("it is not divesible by 3")
+            print("your number is divesible by 2")
+            x = num / 2
+            print(f"{num} / 2 = {x}")
+            print("it is not divesible by 5")
+elif num % 3 == 0 :
+    if num % 5 == 0:
+
         print("your number is divesible by 3")
         y = num / 3
-        print (f"{num} / 3 = {y}")
+        print(f"{num} / 3 = {y}")
+        print("it is not divesible by 2")
+        print("your number is divesible by 5")
+        z = num / 5
+        print(f"{num} / 5 = {z}")
     else:
-        print("it is not divesible by 3")
-        print("your number is divesible by 2")
-        x = num / 2
-        print(f"{num} / 2 = {x}")
-else :
-    if num % 3 == 0 :
         print("your number is divesible by 3")
         y = num / 3
         print (f"{num} / 3 = {y}")
         print("it is not divesible by 2")
-    else :
-        print("it is not divesible by 2 or 3")
+        print("it is not divesible by 5")
+elif num % 5 == 0 :
+    print ("your number is divesible by 5")
+    z = num / 5
+    print(f"{num} / 5 = {z}")
+    print("it is not divesible by 3")
+    print("it is not divesible by 2")
+else :
+    print("it is not divesible by 2 or 3 or 5")
 
 
 
