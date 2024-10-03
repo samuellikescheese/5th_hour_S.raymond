2e830d3d6807c904933a7116bcedbeec91d1556f
samuel_raymond <sr128226@student.wpsok.org>
2024-10-03 13:17:19 -0500

    added HW7.py

---
+++ HW7.py
@@ -0,0 +1,43 @@
+#samuel raymond
+#5th
+#HW6
+
+print("hello world")
+
+wifi = 12
+login = 6
+admin = 7
+
+
+
+
+if wifi == times :
+    print ("good")
+
+
+
+
+print (10 > 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
