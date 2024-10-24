407115e56ef387be297331b3bdc957cb4b262b59
samuel_raymond <sr128226@student.wpsok.org>
2024-10-24 12:54:49 -0500

    added senario 9.py

---
+++ HW9.py
@@ -0,0 +1,44 @@
+#samuel raymond
+#5th
+#HW6
+import random
+
+print("hello world")
+
+temp = random.randint(0,30)
+print (temp)
+if temp > 20:
+    print ("its hot")
+elif temp > 10:
+    print ("its mild")
+else :
+    print ("its cold")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
