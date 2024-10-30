cca75c09eeb8127bc0e831476e83862f8b8f76ec
samuel_raymond <sr128226@student.wpsok.org>
2024-10-30 12:41:06 -0500

    added HW9.py

--- HW9.py
+++ HW9.py
@@ -17,7 +17,10 @@ print ("thank you")
 
 
 
+#1. Print Hello World!
 
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
 
 
 
